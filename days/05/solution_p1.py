071b0a01e6ec0ce0d4c995e27fbb30b473f018e1
Tch1b0 <Jpour3006@gmail.com>
2022-12-05 17:55:39 +0100

    solve day 5 with (non-clean) python code

--- days/05/solution_p1.py
+++ days/05/solution_p1.py
@@ -1,11 +1,9 @@
-import re
-
 with open("days/05/input.txt") as f:
     content = f.readlines()
 
 s = []
 
-for x in [x.replace("    "," [ ] ").strip() for x in content if "move" not in x and x != "\n" and "1" not in x]:
+for x in [x.replace("    ", " [ ] ").strip() for x in content if "move" not in x and x != "\n" and "1" not in x]:
     for i, y in enumerate(x.split("[")):
         if len(s) - 1 < i:
             s.append([])
@@ -14,11 +12,12 @@ for x in [x.replace("    "," [ ] ").strip() for x in content if "move" not in x 
         if y != "":
             s[i].append(y)
 
-s = list(map(lambda x: x[::-1], s[1:]))
+s: list[list[str]] = list(map(lambda x: x[::-1], s[1:]))
 
 for x in [x.replace("move ", "").replace("from ", "").replace("to ", "").split(" ") for x in content if "move" in x]:
     n, fr, to = list(map(lambda x: x - 1, map(int, x)))
-    s[to].append(s[fr][len(s[fr]) - n - 1:])
-    s[fr] = s[fr][:len(s[fr]) - n - 1]
+    for y in range(n + 1):
+        s[to].append(s[fr].pop())
+        print(s)
 
-print([x[0] for x in s])
+print(*[x[-1] for x in s], sep="")
---
+++ days/05/solution_p2.py
@@ -0,0 +1,25 @@
+with open("days/05/input.txt") as f:
+    content = f.readlines()
+
+s = []
+
+for x in [x.replace("    ", " [ ] ").strip() for x in content if "move" not in x and x != "\n" and "1" not in x]:
+    for i, y in enumerate(x.split("[")):
+        if len(s) - 1 < i:
+            s.append([])
+
+        y = y.replace("]", "").strip()
+        if y != "":
+            s[i].append(y)
+
+s: list[list[str]] = list(map(lambda x: x[::-1], s[1:]))
+
+for x in [x.replace("move ", "").replace("from ", "").replace("to ", "").split(" ") for x in content if "move" in x]:
+    n, fr, to = list(map(lambda x: x - 1, map(int, x)))
+    n += 1
+    vals = s[fr][::-1][:n][::-1]
+    s[to] += vals
+    s[fr] = s[fr][::-1][n:][::-1]
+    print(s, vals)
+
+print(*[x[-1] for x in s], sep="")
