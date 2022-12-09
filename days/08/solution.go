ca4a6fa7fc0368fc0a496ae39a136b27c6ce054b
Tch1b0 <jpour3006@gmail.com>
2022-12-09 15:03:39 +0100

    day 8: solve

--- days/08/solution.go
+++ days/08/solution.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Tch1b0/polaris/array"
 	"github.com/Tch1b0/polaris/input"
+	"github.com/Tch1b0/polaris/math"
 	"github.com/Tch1b0/polaris/strings"
 )
 
@@ -30,20 +31,63 @@ func getInput() [][]int {
 }
 
 func swapRowCol(arr [][]int) [][]int {
-	x := arr
-	for i, row := range arr {
+	x := make([][]int, len(arr[0]))
+	for _, row := range arr {
 		for j, col := range row {
-			x[j][i] = col
+			x[j] = append(x[j], col)
 		}
 	}
 
 	return x
 }
 
+func viewScore(grid [][]int, y, x int) int {
+	prod := 1
+	val := grid[y][x]
+
+	v := 0
+	for k := x + 1; k < len(grid[0]); k++ {
+		v += 1
+		if grid[y][k] >= val {
+			break
+		}
+	}
+	prod *= v
+
+	v = 0
+	for k := x - 1; k >= 0; k-- {
+		v += 1
+		if grid[y][k] >= val {
+			break
+		}
+	}
+	prod *= v
+
+	v = 0
+	for k := y + 1; k < len(grid); k++ {
+		v += 1
+		if grid[k][x] >= val {
+			break
+		}
+	}
+	prod *= v
+
+	v = 0
+	for k := y - 1; k >= 0; k-- {
+		v += 1
+		if grid[k][x] >= val {
+			break
+		}
+	}
+	prod *= v
+
+	return prod
+}
+
 func part1() int {
 	grid := getInput()
 	xGrid := swapRowCol(grid)
-	sum := len(grid)*2 + len(grid[0])*2 - 4
+	sum := len(grid)*2 + (len(xGrid)-2)*2
 	for i, row := range grid {
 		if i == 0 || i == len(grid)-1 {
 			continue
@@ -55,7 +99,7 @@ func part1() int {
 			}
 
 			isLower := func(v, _ int) bool { return v < col }
-			if array.All(grid[i][:j-1], isLower) || array.All(grid[i][j+1:], isLower) || array.All(xGrid[i][:j-1], isLower) || array.All(xGrid[i][j+1:], isLower) {
+			if array.All(grid[i][:j], isLower) || array.All(grid[i][j+1:], isLower) || array.All(xGrid[j][:i], isLower) || array.All(xGrid[j][i+1:], isLower) {
 				sum += 1
 			}
 		}
@@ -64,7 +108,25 @@ func part1() int {
 }
 
 func part2() int {
-	return -1
+	scores := []int{}
+
+	grid := getInput()
+
+	for i, row := range grid {
+		if i == 0 || i == len(grid)-1 {
+			continue
+		}
+
+		for j, _ := range row {
+			if j == 0 || j == len(row)-1 {
+				continue
+			}
+
+			scores = append(scores, viewScore(grid, i, j))
+		}
+	}
+
+	return math.Max(scores)
 }
 
 func main() {
